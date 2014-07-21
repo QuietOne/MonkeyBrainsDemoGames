c85ca4827374b0736236a955413b90e2a1d09bb2
QuietOne <tihomir.radosavljevic@gmail.com>
2014-07-21 20:00:00 +0200

    game updated

---
+++ RobotFightGame/src/behaviours/FleeInsideTerrain.java
@@ -0,0 +1,41 @@
+package behaviours;
+
+import com.jme3.ai.agents.Agent;
+import com.jme3.ai.agents.behaviours.npc.steering.FleeBehaviour;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+/**
+ *
+ * @author Tihomir Radosavljević
+ */
+public class FleeInsideTerrain extends FleeBehaviour{
+
+    private float terrainSize;
+
+    public FleeInsideTerrain(float terrainSize, Agent agent, Agent target) {
+        super(agent, target);
+        this.terrainSize = terrainSize;
+    }
+
+    public FleeInsideTerrain(float terrainSize, Agent agent, Agent target, Spatial spatial) {
+        super(agent, target, spatial);
+        this.terrainSize = terrainSize;
+    }
+    
+    public FleeInsideTerrain(Agent agent, Agent target) {
+        super(agent, target);
+    }
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        Vector3f oldPos = agent.getLocalTranslation().clone();
+        super.controlUpdate(tpf);
+        if (agent.getLocalTranslation().x > terrainSize*2 || agent.getLocalTranslation().z > terrainSize*2
+                || agent.getLocalTranslation().x < -terrainSize*2 || agent.getLocalTranslation().z < -terrainSize*2) {
+            agent.setLocalTranslation(oldPos);
+        }
+    }
+
+    
+}
---
+++ RobotFightGame/src/simulation/evolution/events/BeingAttackedEventListener.java
@@ -0,0 +1,9 @@
+package simulation.evolution.events;
+
+/**
+ *
+ * @author Tihomir Radosavljević
+ */
+public interface BeingAttackedEventListener {
+    public void handleBeingAttackedEvent(BeingAttackedEvent event);
+}
