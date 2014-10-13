a2ad123b6bdd8250f624e320887c8b18dd658d2a
QuietOne <tihomir.radosavljevic@gmail.com>
2014-10-13 17:55:13 +0200

    obstacle avoidance demo works

--- SteeringBehaviors/src/steeringDemos/demos/ObstacleAvoidanceDemo.java
+++ SteeringBehaviors/src/steeringDemos/demos/ObstacleAvoidanceDemo.java
@@ -1,4 +1,32 @@
-//Copyright (c) 2014, Jesús Martín Berlanga. All rights reserved. Distributed under the BSD licence. Read "com/jme3/ai/license.txt".
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package steeringDemos.demos;
 
 import com.jme3.ai.agents.Agent;
@@ -8,16 +36,13 @@ import com.jme3.ai.agents.behaviors.npc.steering.CompoundSteeringBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.ObstacleAvoidanceBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.SeekBehavior;
 import com.jme3.ai.agents.util.GameEntity;
-
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.math.FastMath;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
 import steeringDemos.BasicDemo;
 import steeringDemos.control.CustomSteerControl;
 
@@ -25,7 +50,7 @@ import steeringDemos.control.CustomSteerControl;
  * Obstacle avoidance demo
  *
  * @author Jesús Martín Berlanga
- * @version 2.0
+ * @version 2.0.0
  */
 public class ObstacleAvoidanceDemo extends BasicDemo {
 
@@ -44,57 +69,56 @@ public class ObstacleAvoidanceDemo extends BasicDemo {
         this.steerControl.setCameraSettings(getCamera());
         this.steerControl.setFlyCameraSettings(getFlyByCamera());
 
-        //defining rootNode for aiAppState processing
-        aiAppState.setApp(this);
-        aiAppState.setGameControl(this.steerControl);
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
+        brainsAppState.setGameControl(this.steerControl);
 
         Vector3f[] spawnArea = null;
         this.numberNeighbours = 150;
 
         agent = this.createBoid("Target", ColorRGBA.Blue, 0.27f);
 
-        aiAppState.addAgent(agent); //Add the target to the aiAppState
+        brainsAppState.addAgent(agent); //Add the target to the brainsAppState
         this.setStats(
                 agent,
                 this.targetMoveSpeed,
                 this.targetRotationSpeed,
                 this.targetMass,
                 this.targetMaxForce);
-        aiAppState.getGameControl().spawn(agent, new Vector3f());
+        brainsAppState.getGameControl().spawn(agent, new Vector3f());
 
         Agent[] neighbours = new Agent[this.numberNeighbours];
 
         for (int i = 0; i < this.numberNeighbours; i++) {
             neighbours[i] = this.createSphere("neighbour_" + i, ColorRGBA.Orange, 0.4f);
 
-            aiAppState.addAgent(neighbours[i]); //Add the neighbours to the aiAppState
+            brainsAppState.addAgent(neighbours[i]); //Add the neighbours to the brainsAppState
             this.setStats(
                     neighbours[i],
                     this.neighboursMoveSpeed,
                     this.neighboursRotationSpeed,
                     this.neighboursMass,
                     this.neighboursMaxForce);
-            aiAppState.getGameControl().spawn(neighbours[i], spawnArea);
+            brainsAppState.getGameControl().spawn(neighbours[i], spawnArea);
 
             SimpleMainBehavior mainB = new SimpleMainBehavior(neighbours[i]);
-            neighbours[i].setMainBehaviour(mainB);
+            neighbours[i].setMainBehavior(mainB);
         }
 
         focus = this.createSphere("focus", ColorRGBA.Green, 0.4f);
-        aiAppState.addAgent(focus);
+        brainsAppState.addAgent(focus);
 
-        aiAppState.addAgent(focus); //Add the neighbours to the aiAppState
+        brainsAppState.addAgent(focus); //Add the neighbours to the brainsAppState
         this.setStats(
                 focus,
                 this.neighboursMoveSpeed,
                 this.neighboursRotationSpeed,
                 this.neighboursMass,
                 this.neighboursMaxForce);
-        aiAppState.getGameControl().spawn(focus, this.generateRandomPosition());
+        brainsAppState.getGameControl().spawn(focus, this.generateRandomPosition());
 
         SimpleMainBehavior mainB = new SimpleMainBehavior(focus);
-        focus.setMainBehaviour(mainB);
-
+        focus.setMainBehavior(mainB);
 
         List<GameEntity> obstacles = new ArrayList<GameEntity>();
         obstacles.addAll(Arrays.asList(neighbours));
@@ -112,14 +136,14 @@ public class ObstacleAvoidanceDemo extends BasicDemo {
         steer.addSteerBehavior(seekSteer);
         steer.addSteerBehavior(obstacleAvoidance);
         targetMainB.addBehavior(steer);
-        agent.setMainBehaviour(targetMainB);
+        agent.setMainBehavior(targetMainB);
 
-        aiAppState.start();
+        brainsAppState.start();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        aiAppState.update(tpf);
+        brainsAppState.update(tpf);
 
         if (this.agent.distanceRelativeToGameEntity(this.focus) < 0.5f) {
             this.focus.setLocalTranslation(this.generateRandomPosition());
