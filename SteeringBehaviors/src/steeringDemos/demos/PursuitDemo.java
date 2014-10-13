dd2ec404e673da25a67ba8e7197f9d76be3efc18
QuietOne <tihomir.radosavljevic@gmail.com>
2014-10-13 18:02:10 +0200

    pursuit demo works

--- SteeringBehaviors/src/steeringDemos/demos/PursuitDemo.java
+++ SteeringBehaviors/src/steeringDemos/demos/PursuitDemo.java
@@ -1,9 +1,36 @@
-//Copyright (c) 2014, Jesús Martín Berlanga. All rights reserved. Distributed under the BSD licence. Read "com/jme3/ai/license.txt".
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package steeringDemos.demos;
 
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-
 import com.jme3.ai.agents.Agent;
 import com.jme3.ai.agents.behaviors.npc.steering.SeparationBehavior;
 import com.jme3.ai.agents.behaviors.npc.SimpleMainBehavior;
@@ -13,16 +40,13 @@ import com.jme3.ai.agents.behaviors.npc.steering.MoveBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.PursuitBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.WanderBehavior;
 import com.jme3.ai.agents.util.GameEntity;
-
 import com.jme3.font.BitmapText;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
-
 import steeringDemos.BasicDemo;
 import steeringDemos.control.CustomSteerControl;
-
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
@@ -33,20 +57,20 @@ import java.util.Arrays;
  * Pursuit demo
  *
  * @author Jesús Martín Berlanga
- * @version 2.0
+ * @version 2.0.0
  */
 public class PursuitDemo extends BasicDemo {
 
     private SeparationBehavior separation[];
     private CompoundSteeringBehavior targetSteer;
-    Agent target;
+    private Agent target;
     private boolean isStrengthScalar = true;
     private float scalarStrength = 0.1f;
     private BitmapText scalarStrengthHudText;
     private BitmapText scalarInfoHudtext;
-    private final String ESCALAR_INFO_HUD_MESSAGE = "Press H to increase the separation escalar strength, L to decrease.";
-    MoveBehavior targetMoveBehavior;
-    WanderBehavior targetWanderBehavior;
+    private final String SCALAR_INFO_HUD_MESSAGE = "Press H to increase the separation scalar strength, L to decrease.";
+    private MoveBehavior targetMoveBehavior;
+    private WanderBehavior targetWanderBehavior;
     private boolean turnDinamicMode = true;
     private java.awt.event.ActionListener changeDinamicMode = new java.awt.event.ActionListener() {
         public void actionPerformed(ActionEvent event) {
@@ -81,7 +105,7 @@ public class PursuitDemo extends BasicDemo {
         // font color
         hudText.setColor(ColorRGBA.Red);
         // the text
-        hudText.setText("Press N to switch betwen escalar 'separation strength' and 'plane strength'.");
+        hudText.setText("Press N to switch betwen scalar 'separation strength' and 'plane strength'.");
         // position
         hudText.setLocalTranslation(0, 475, 0);
         guiNode.attachChild(hudText);
@@ -103,7 +127,7 @@ public class PursuitDemo extends BasicDemo {
         // font color
         this.scalarInfoHudtext.setColor(ColorRGBA.Orange);
         // the text
-        this.scalarInfoHudtext.setText(this.ESCALAR_INFO_HUD_MESSAGE);
+        this.scalarInfoHudtext.setText(this.SCALAR_INFO_HUD_MESSAGE);
         // position
         this.scalarInfoHudtext.setLocalTranslation(0, 450, 0);
         guiNode.attachChild(scalarInfoHudtext);
@@ -112,45 +136,45 @@ public class PursuitDemo extends BasicDemo {
         this.steerControl.setCameraSettings(getCamera());
         this.steerControl.setFlyCameraSettings(getFlyByCamera());
 
-        //defining rootNode for aiAppState processing
-        aiAppState.setApp(this);
-        aiAppState.setGameControl(this.steerControl);
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
+        brainsAppState.setGameControl(this.steerControl);
 
         Vector3f[] spawnArea = null;
         this.numberNeighbours = 25;
 
         target = this.createBoid("Target", this.targetColor, 0.11f);
 
-        //Add the target to the aiAppState
-        aiAppState.addAgent(target);
+        //Add the target to the brainsAppState
+        brainsAppState.addAgent(target);
         this.setStats(
                 target,
                 this.targetMoveSpeed,
                 this.targetRotationSpeed,
                 this.targetMass,
                 this.targetMaxForce);
-        aiAppState.getGameControl().spawn(target, new Vector3f());
+        brainsAppState.getGameControl().spawn(target, new Vector3f());
 
         Agent[] neighbours = new Agent[this.numberNeighbours];
 
         for (int i = 0; i < this.numberNeighbours; i++) {
             neighbours[i] = this.createBoid("Neighbour " + i, this.neighboursColor, 0.11f);
-            //Add the neighbours to the aiAppState
-            aiAppState.addAgent(neighbours[i]);
+            //Add the neighbours to the brainsAppState
+            brainsAppState.addAgent(neighbours[i]);
             this.setStats(
                     neighbours[i],
                     this.neighboursMoveSpeed,
                     this.neighboursRotationSpeed,
                     this.neighboursMass,
                     this.neighboursMaxForce);
-            aiAppState.getGameControl().spawn(neighbours[i], spawnArea);
+            brainsAppState.getGameControl().spawn(neighbours[i], spawnArea);
         }
 
         List<GameEntity> obstacles = new ArrayList<GameEntity>();
         obstacles.addAll(Arrays.asList(neighbours));
 
         //adding behaviours to target
-        SimpleMainBehavior targetMainBehaviour = new SimpleMainBehavior(target);
+        SimpleMainBehavior targetMainBehavior = new SimpleMainBehavior(target);
         targetMoveBehavior = new MoveBehavior(target);
         targetMoveBehavior.setupStrengthControl(0.25f);
         targetWanderBehavior = new WanderBehavior(target);
@@ -167,16 +191,16 @@ public class PursuitDemo extends BasicDemo {
 
         targetMoveBehavior.setMoveDirection(new Vector3f(randomDistance, randomDistance, randomDistance));
 
-        targetMainBehaviour.addBehavior(targetSteer);
-        target.setMainBehaviour(targetMainBehaviour);
+        targetMainBehavior.addBehavior(targetSteer);
+        target.setMainBehavior(targetMainBehavior);
 
-        SimpleMainBehavior[] neighboursMainBehaviour = new SimpleMainBehavior[neighbours.length];
+        SimpleMainBehavior[] neighboursMainBehavior = new SimpleMainBehavior[neighbours.length];
 
         separation = new SeparationBehavior[neighbours.length];
 
         //adding pursuit behaviours to agents
         for (int i = 0; i < neighbours.length; i++) {
-            neighboursMainBehaviour[i] = new SimpleMainBehavior(neighbours[i]);
+            neighboursMainBehavior[i] = new SimpleMainBehavior(neighbours[i]);
 
             PursuitBehavior pursuit = new PursuitBehavior(
                     neighbours[i],
@@ -188,20 +212,20 @@ public class PursuitDemo extends BasicDemo {
             BalancedCompoundSteeringBehavior neighSteer = new BalancedCompoundSteeringBehavior(neighbours[i]);
             neighSteer.addSteerBehavior(separation[i]);
             neighSteer.addSteerBehavior(pursuit);
-            neighboursMainBehaviour[i].addBehavior(neighSteer);
-            neighbours[i].setMainBehaviour(neighboursMainBehaviour[i]);
+            neighboursMainBehavior[i].addBehavior(neighSteer);
+            neighbours[i].setMainBehavior(neighboursMainBehavior[i]);
         }
 
-        aiAppState.start();
+        brainsAppState.start();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        aiAppState.update(tpf);
+        brainsAppState.update(tpf);
 
         if (this.isStrengthScalar) {
             scalarStrengthHudText.setText(String.valueOf(scalarStrength));
-            this.scalarInfoHudtext.setText(this.ESCALAR_INFO_HUD_MESSAGE);
+            this.scalarInfoHudtext.setText(this.SCALAR_INFO_HUD_MESSAGE);
         } else {
             scalarStrengthHudText.setText("");
             this.scalarInfoHudtext.setText("");
@@ -224,7 +248,6 @@ public class PursuitDemo extends BasicDemo {
     }
     private ActionListener actionListener = new ActionListener() {
         public void onAction(String name, boolean keyPressed, float tpf) {
-
             if (name.equals("Switch mode") && !keyPressed) {
                 changeMode();
             }
@@ -233,22 +256,17 @@ public class PursuitDemo extends BasicDemo {
     };
 
     private void changeMode() {
-
         if (this.isStrengthScalar) {
-            for (SeparationBehavior behaviour : this.separation) {
-                behaviour.setupStrengthControl(1, 0, 1);
+            for (SeparationBehavior behavior : this.separation) {
+                behavior.setupStrengthControl(1, 0, 1);
             }
-
             targetSteer.setupStrengthControl(1, 0, 1);
-
             this.isStrengthScalar = false;
         } else {
-            for (SeparationBehavior behaviour : this.separation) {
-                behaviour.setupStrengthControl(scalarStrength);
+            for (SeparationBehavior behavior : this.separation) {
+                behavior.setupStrengthControl(scalarStrength);
             }
-
             targetSteer.turnOffStrengthControl();
-
             this.isStrengthScalar = true;
         }
     }
@@ -265,30 +283,23 @@ public class PursuitDemo extends BasicDemo {
     };
 
     private void increaseSeparation() {
-
         if (this.isStrengthScalar) {
             this.scalarStrength = this.scalarStrength + 0.05f;
-
             for (SeparationBehavior behaviour : this.separation) {
                 behaviour.setupStrengthControl(this.scalarStrength);
             }
         }
-
     }
 
     private void decreaseSeparation() {
-
         if (this.isStrengthScalar) {
             this.scalarStrength = this.scalarStrength - 0.075f;
-
             if (this.scalarStrength < 0) {
                 this.scalarStrength = 0;
             }
-
-            for (SeparationBehavior behaviour : this.separation) {
-                behaviour.setupStrengthControl(this.scalarStrength);
+            for (SeparationBehavior behavior : this.separation) {
+                behavior.setupStrengthControl(this.scalarStrength);
             }
         }
-
     }
 }
