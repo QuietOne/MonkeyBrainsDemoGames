d5316d5ef7b44dbc92f633454342a8b7522be527
QuietOne <tihomir.radosavljevic@gmail.com>
2014-10-13 18:21:02 +0200

    seek demo works

--- SteeringBehaviors/src/steeringDemos/demos/SeekDemo.java
+++ SteeringBehaviors/src/steeringDemos/demos/SeekDemo.java
@@ -1,4 +1,32 @@
-//Copyright (c) 2014, Jesús Martín Berlanga. All rights reserved. Distributed under the BSD licence. Read "com/jme3/ai/license.txt".
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package steeringDemos.demos;
 
 import com.jme3.ai.agents.Agent;
@@ -19,7 +47,7 @@ public class SeekDemo extends BasicDemo {
 
     private Agent target;
     private SeekBehavior targetMove;
-    Vector3f[] locations = new Vector3f[]{
+    private Vector3f[] locations = new Vector3f[]{
         new Vector3f(7, 0, 0),
         new Vector3f(0, 7, 0),
         new Vector3f(0, 0, 7)
@@ -37,9 +65,9 @@ public class SeekDemo extends BasicDemo {
         this.steerControl.setCameraSettings(getCamera());
         this.steerControl.setFlyCameraSettings(getFlyByCamera());
 
-        //defining rootNode for aiAppState processing
-        aiAppState.setApp(this);
-        aiAppState.setGameControl(new CustomSteerControl(5f));
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
+        brainsAppState.setGameControl(new CustomSteerControl(5f));
 
         target = this.createBoid("Target", this.targetColor, 0.11f);
 
@@ -47,8 +75,8 @@ public class SeekDemo extends BasicDemo {
             this.createSphereHelper("Sphere " + loc.toString(), ColorRGBA.Yellow, 0.05f, loc);
         }
 
-        aiAppState.addAgent(target); //Add the target to the aiAppState
-        aiAppState.getGameControl().spawn(target, Vector3f.ZERO);
+        brainsAppState.addAgent(target); //Add the target to the brainsAppState
+        brainsAppState.getGameControl().spawn(target, Vector3f.ZERO);
         this.setStats(
                 target,
                 this.targetMoveSpeed,
@@ -56,28 +84,25 @@ public class SeekDemo extends BasicDemo {
                 this.targetMass,
                 this.targetMaxForce);
 
-        SimpleMainBehavior targetMainBehaviour = new SimpleMainBehavior(target);
+        SimpleMainBehavior targetMainBehavior = new SimpleMainBehavior(target);
 
         this.targetMove = new SeekBehavior(target, this.locations[0]);
         this.currentFocus = 0;
-        targetMainBehaviour.addBehavior(this.targetMove);
+        targetMainBehavior.addBehavior(this.targetMove);
 
-        target.setMainBehaviour(targetMainBehaviour);
+        target.setMainBehavior(targetMainBehavior);
 
-        aiAppState.start();
+        brainsAppState.start();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        aiAppState.update(tpf);
-
+        brainsAppState.update(tpf);
         if (this.target.distanceFromPosition(this.locations[this.currentFocus]) < 0.01f) {
             this.currentFocus++;
-
             if (this.currentFocus > this.locations.length - 1) {
                 this.currentFocus = 0;
             }
-
             this.targetMove.setSeekingPosition(this.locations[this.currentFocus]);
         }
     }
