50a902a18c5127c6e6d7bdda9aec4f06cc3e1a04
QuietOne <tihomir.radosavljevic@gmail.com>
2014-10-13 17:38:13 +0200

    Aligment demo works

--- SteeringBehaviors/src/steeringDemos/demos/AlignmentDemo.java
+++ SteeringBehaviors/src/steeringDemos/demos/AlignmentDemo.java
@@ -1,10 +1,36 @@
-//Copyright (c) 2014, Jesús Martín Berlanga. All rights reserved.
-//Distributed under the BSD licence. Read "com/jme3/ai/license.txt".
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package steeringDemos.demos;
 
 import steeringDemos.BasicDemo;
 import steeringDemos.control.CustomSteerControl;
-
 import com.jme3.ai.agents.Agent;
 import com.jme3.ai.agents.behaviors.npc.steering.SeparationBehavior;
 import com.jme3.ai.agents.behaviors.npc.SimpleMainBehavior;
@@ -12,10 +38,8 @@ import com.jme3.ai.agents.behaviors.npc.steering.AlignmentBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.CompoundSteeringBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.WanderBehavior;
 import com.jme3.ai.agents.util.GameEntity;
-
 import com.jme3.math.Vector3f;
 import com.jme3.math.FastMath;
-
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -24,7 +48,7 @@ import java.util.ArrayList;
  * Alignment Demo
  *
  * @author Jesús Martín Berlanga
- * @version 2.0
+ * @version 2.0.0
  */
 public class AlignmentDemo extends BasicDemo {
 
@@ -40,10 +64,10 @@ public class AlignmentDemo extends BasicDemo {
         this.steerControl.setCameraSettings(getCamera());
         this.steerControl.setFlyCameraSettings(getFlyByCamera());
 
-        //defining rootNode for aiAppState processing
-        aiAppState.setApp(this);
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
         this.numberNeighbours = 150;
-        aiAppState.setGameControl(this.steerControl);
+        brainsAppState.setGameControl(this.steerControl);
 
         Vector3f[] spawnArea = null;
 
@@ -51,27 +75,27 @@ public class AlignmentDemo extends BasicDemo {
 
         for (int i = 0; i < this.numberNeighbours; i++) {
             boids[i] = this.createBoid("boid " + i, this.neighboursColor, 0.1f);
-            aiAppState.addAgent(boids[i]); //Add the neighbours to the aiAppState
+            brainsAppState.addAgent(boids[i]); //Add the neighbours to the brainsAppState
             this.setStats(
                     boids[i],
                     this.neighboursMoveSpeed,
                     this.neighboursRotationSpeed,
                     this.neighboursMass,
                     this.neighboursMaxForce);
-            aiAppState.getGameControl().spawn(boids[i], spawnArea);
+            brainsAppState.getGameControl().spawn(boids[i], spawnArea);
         }
 
         List<GameEntity> obstacles = new ArrayList<GameEntity>();
         obstacles.addAll(Arrays.asList(boids));
 
-        SimpleMainBehavior[] neighboursMainBehaviour = new SimpleMainBehavior[boids.length];
+        SimpleMainBehavior[] neighboursMainBehavior = new SimpleMainBehavior[boids.length];
 
         SeparationBehavior[] separation = new SeparationBehavior[boids.length];
         AlignmentBehavior[] alignment = new AlignmentBehavior[boids.length];
         WanderBehavior[] wander = new WanderBehavior[boids.length];
 
         for (int i = 0; i < boids.length; i++) {
-            neighboursMainBehaviour[i] = new SimpleMainBehavior(boids[i]);
+            neighboursMainBehavior[i] = new SimpleMainBehavior(boids[i]);
 
             separation[i] = new SeparationBehavior(boids[i], obstacles);
             alignment[i] = new AlignmentBehavior(boids[i], obstacles, 5f, FastMath.PI / 4);
@@ -88,16 +112,16 @@ public class AlignmentDemo extends BasicDemo {
             steer.addSteerBehavior(alignment[i]);
             steer.addSteerBehavior(separation[i]);
             steer.addSteerBehavior(wander[i]);
-            neighboursMainBehaviour[i].addBehavior(steer);
+            neighboursMainBehavior[i].addBehavior(steer);
 
-            boids[i].setMainBehaviour(neighboursMainBehaviour[i]);
+            boids[i].setMainBehavior(neighboursMainBehavior[i]);
         }
 
-        aiAppState.start();
+        brainsAppState.start();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        aiAppState.update(tpf);
+        brainsAppState.update(tpf);
     }
 }
