f7f136959e147ed7ce8976fb2e92e008a5451ffa
QuietOne <tihomir.radosavljevic@gmail.com>
2014-10-13 17:53:37 +0200

    move demo works

--- SteeringBehaviors/src/steeringDemos/demos/MoveDemo.java
+++ SteeringBehaviors/src/steeringDemos/demos/MoveDemo.java
@@ -1,13 +1,39 @@
-//Copyright (c) 2014, Jesús Martín Berlanga. All rights reserved. Distributed under the BSD licence. Read "com/jme3/ai/license.txt".
+/**
+ * Copyright (c) 2014, jMonkeyEngine All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of 'jMonkeyEngine' nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package steeringDemos.demos;
 
 import com.jme3.ai.agents.Agent;
 import com.jme3.ai.agents.behaviors.npc.SimpleMainBehavior;
 import com.jme3.ai.agents.behaviors.npc.steering.MoveBehavior;
 import com.jme3.math.FastMath;
-
 import com.jme3.math.Vector3f;
-
 import steeringDemos.BasicDemo;
 import steeringDemos.control.CustomSteerControl;
 
@@ -15,7 +41,7 @@ import steeringDemos.control.CustomSteerControl;
  * Move Demo
  *
  * @author Jesús Martín Berlanga
- * @version 1.0
+ * @version 1.0.0
  */
 public class MoveDemo extends BasicDemo {
 
@@ -30,14 +56,14 @@ public class MoveDemo extends BasicDemo {
         this.steerControl.setCameraSettings(getCamera());
         this.steerControl.setFlyCameraSettings(getFlyByCamera());
 
-        //defining rootNode for aiAppState processing
-        aiAppState.setApp(this);
-        aiAppState.setGameControl(new CustomSteerControl(5f));
+        //defining rootNode for brainsAppState processing
+        brainsAppState.setApp(this);
+        brainsAppState.setGameControl(new CustomSteerControl(5f));
 
         Agent target = this.createBoid("Target", this.targetColor, 0.11f);
 
-        aiAppState.addAgent(target); //Add the target to the aiAppState
-        aiAppState.getGameControl().spawn(target, Vector3f.ZERO);
+        brainsAppState.addAgent(target); //Add the target to the brainsAppState
+        brainsAppState.getGameControl().spawn(target, Vector3f.ZERO);
         this.setStats(
                 target,
                 this.targetMoveSpeed,
@@ -45,7 +71,7 @@ public class MoveDemo extends BasicDemo {
                 this.targetMass,
                 this.targetMaxForce);
 
-        SimpleMainBehavior targetMainBehaviour = new SimpleMainBehavior(target);
+        SimpleMainBehavior targetMainBehavior = new SimpleMainBehavior(target);
 
         MoveBehavior targetMoveBehavior = new MoveBehavior(target);
         targetMoveBehavior.setMoveDirection(new Vector3f(
@@ -53,14 +79,14 @@ public class MoveDemo extends BasicDemo {
                 FastMath.nextRandomFloat() - 0.5f,
                 FastMath.nextRandomFloat() - 0.5f));
 
-        targetMainBehaviour.addBehavior(targetMoveBehavior);
-        target.setMainBehaviour(targetMainBehaviour);
+        targetMainBehavior.addBehavior(targetMoveBehavior);
+        target.setMainBehavior(targetMainBehavior);
 
-        aiAppState.start();
+        brainsAppState.start();
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        aiAppState.update(tpf);
+        brainsAppState.update(tpf);
     }
 }
